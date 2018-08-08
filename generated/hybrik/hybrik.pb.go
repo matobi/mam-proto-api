20ac653c4012920c80bb735f095bb37b53b57820
Mattias Tobiasson <mattias.tobiasson@viaplay.com>
2018-08-08 18:06:26 +0200

    many renames

--- generated/hybrik/hybrik.pb.go
+++ generated/hybrik/hybrik.pb.go
@@ -23,76 +23,76 @@ var _ = math.Inf
 // proto package needs to be updated.
 const _ = proto.ProtoPackageIsVersion2 // please upgrade the proto package
 
-type JobRequest struct {
+type JobIn struct {
 	JobId                string   `protobuf:"bytes,1,opt,name=job_id,json=jobId,proto3" json:"job_id,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
 	XXX_sizecache        int32    `json:"-"`
 }
 
-func (m *JobRequest) Reset()         { *m = JobRequest{} }
-func (m *JobRequest) String() string { return proto.CompactTextString(m) }
-func (*JobRequest) ProtoMessage()    {}
-func (*JobRequest) Descriptor() ([]byte, []int) {
-	return fileDescriptor_hybrik_552458008a7cdb9c, []int{0}
+func (m *JobIn) Reset()         { *m = JobIn{} }
+func (m *JobIn) String() string { return proto.CompactTextString(m) }
+func (*JobIn) ProtoMessage()    {}
+func (*JobIn) Descriptor() ([]byte, []int) {
+	return fileDescriptor_hybrik_8fee990f0d0ce65a, []int{0}
 }
-func (m *JobRequest) XXX_Unmarshal(b []byte) error {
-	return xxx_messageInfo_JobRequest.Unmarshal(m, b)
+func (m *JobIn) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_JobIn.Unmarshal(m, b)
 }
-func (m *JobRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	return xxx_messageInfo_JobRequest.Marshal(b, m, deterministic)
+func (m *JobIn) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_JobIn.Marshal(b, m, deterministic)
 }
-func (dst *JobRequest) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_JobRequest.Merge(dst, src)
+func (dst *JobIn) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_JobIn.Merge(dst, src)
 }
-func (m *JobRequest) XXX_Size() int {
-	return xxx_messageInfo_JobRequest.Size(m)
+func (m *JobIn) XXX_Size() int {
+	return xxx_messageInfo_JobIn.Size(m)
 }
-func (m *JobRequest) XXX_DiscardUnknown() {
-	xxx_messageInfo_JobRequest.DiscardUnknown(m)
+func (m *JobIn) XXX_DiscardUnknown() {
+	xxx_messageInfo_JobIn.DiscardUnknown(m)
 }
 
-var xxx_messageInfo_JobRequest proto.InternalMessageInfo
+var xxx_messageInfo_JobIn proto.InternalMessageInfo
 
-func (m *JobRequest) GetJobId() string {
+func (m *JobIn) GetJobId() string {
 	if m != nil {
 		return m.JobId
 	}
 	return ""
 }
 
-type JobResponse struct {
+type JobOut struct {
 	Job                  *Job     `protobuf:"bytes,1,opt,name=job,proto3" json:"job,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
 	XXX_sizecache        int32    `json:"-"`
 }
 
-func (m *JobResponse) Reset()         { *m = JobResponse{} }
-func (m *JobResponse) String() string { return proto.CompactTextString(m) }
-func (*JobResponse) ProtoMessage()    {}
-func (*JobResponse) Descriptor() ([]byte, []int) {
-	return fileDescriptor_hybrik_552458008a7cdb9c, []int{1}
+func (m *JobOut) Reset()         { *m = JobOut{} }
+func (m *JobOut) String() string { return proto.CompactTextString(m) }
+func (*JobOut) ProtoMessage()    {}
+func (*JobOut) Descriptor() ([]byte, []int) {
+	return fileDescriptor_hybrik_8fee990f0d0ce65a, []int{1}
 }
-func (m *JobResponse) XXX_Unmarshal(b []byte) error {
-	return xxx_messageInfo_JobResponse.Unmarshal(m, b)
+func (m *JobOut) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_JobOut.Unmarshal(m, b)
 }
-func (m *JobResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	return xxx_messageInfo_JobResponse.Marshal(b, m, deterministic)
+func (m *JobOut) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_JobOut.Marshal(b, m, deterministic)
 }
-func (dst *JobResponse) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_JobResponse.Merge(dst, src)
+func (dst *JobOut) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_JobOut.Merge(dst, src)
 }
-func (m *JobResponse) XXX_Size() int {
-	return xxx_messageInfo_JobResponse.Size(m)
+func (m *JobOut) XXX_Size() int {
+	return xxx_messageInfo_JobOut.Size(m)
 }
-func (m *JobResponse) XXX_DiscardUnknown() {
-	xxx_messageInfo_JobResponse.DiscardUnknown(m)
+func (m *JobOut) XXX_DiscardUnknown() {
+	xxx_messageInfo_JobOut.DiscardUnknown(m)
 }
 
-var xxx_messageInfo_JobResponse proto.InternalMessageInfo
+var xxx_messageInfo_JobOut proto.InternalMessageInfo
 
-func (m *JobResponse) GetJob() *Job {
+func (m *JobOut) GetJob() *Job {
 	if m != nil {
 		return m.Job
 	}
@@ -115,7 +115,7 @@ func (m *Job) Reset()         { *m = Job{} }
 func (m *Job) String() string { return proto.CompactTextString(m) }
 func (*Job) ProtoMessage()    {}
 func (*Job) Descriptor() ([]byte, []int) {
-	return fileDescriptor_hybrik_552458008a7cdb9c, []int{2}
+	return fileDescriptor_hybrik_8fee990f0d0ce65a, []int{2}
 }
 func (m *Job) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_Job.Unmarshal(m, b)
@@ -177,7 +177,7 @@ func (m *Job) GetProgress() int32 {
 	return 0
 }
 
-type CreateJobRequest struct {
+type CreateJobIn struct {
 	Dir                  string   `protobuf:"bytes,1,opt,name=dir,proto3" json:"dir,omitempty"`
 	File                 string   `protobuf:"bytes,2,opt,name=file,proto3" json:"file,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
@@ -185,76 +185,76 @@ type CreateJobRequest struct {
 	XXX_sizecache        int32    `json:"-"`
 }
 
-func (m *CreateJobRequest) Reset()         { *m = CreateJobRequest{} }
-func (m *CreateJobRequest) String() string { return proto.CompactTextString(m) }
-func (*CreateJobRequest) ProtoMessage()    {}
-func (*CreateJobRequest) Descriptor() ([]byte, []int) {
-	return fileDescriptor_hybrik_552458008a7cdb9c, []int{3}
+func (m *CreateJobIn) Reset()         { *m = CreateJobIn{} }
+func (m *CreateJobIn) String() string { return proto.CompactTextString(m) }
+func (*CreateJobIn) ProtoMessage()    {}
+func (*CreateJobIn) Descriptor() ([]byte, []int) {
+	return fileDescriptor_hybrik_8fee990f0d0ce65a, []int{3}
 }
-func (m *CreateJobRequest) XXX_Unmarshal(b []byte) error {
-	return xxx_messageInfo_CreateJobRequest.Unmarshal(m, b)
+func (m *CreateJobIn) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_CreateJobIn.Unmarshal(m, b)
 }
-func (m *CreateJobRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	return xxx_messageInfo_CreateJobRequest.Marshal(b, m, deterministic)
+func (m *CreateJobIn) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_CreateJobIn.Marshal(b, m, deterministic)
 }
-func (dst *CreateJobRequest) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_CreateJobRequest.Merge(dst, src)
+func (dst *CreateJobIn) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_CreateJobIn.Merge(dst, src)
 }
-func (m *CreateJobRequest) XXX_Size() int {
-	return xxx_messageInfo_CreateJobRequest.Size(m)
+func (m *CreateJobIn) XXX_Size() int {
+	return xxx_messageInfo_CreateJobIn.Size(m)
 }
-func (m *CreateJobRequest) XXX_DiscardUnknown() {
-	xxx_messageInfo_CreateJobRequest.DiscardUnknown(m)
+func (m *CreateJobIn) XXX_DiscardUnknown() {
+	xxx_messageInfo_CreateJobIn.DiscardUnknown(m)
 }
 
-var xxx_messageInfo_CreateJobRequest proto.InternalMessageInfo
+var xxx_messageInfo_CreateJobIn proto.InternalMessageInfo
 
-func (m *CreateJobRequest) GetDir() string {
+func (m *CreateJobIn) GetDir() string {
 	if m != nil {
 		return m.Dir
 	}
 	return ""
 }
 
-func (m *CreateJobRequest) GetFile() string {
+func (m *CreateJobIn) GetFile() string {
 	if m != nil {
 		return m.File
 	}
 	return ""
 }
 
-type CreateJobResponse struct {
+type CreateJobOut struct {
 	JobId                string   `protobuf:"bytes,1,opt,name=job_id,json=jobId,proto3" json:"job_id,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
 	XXX_sizecache        int32    `json:"-"`
 }
 
-func (m *CreateJobResponse) Reset()         { *m = CreateJobResponse{} }
-func (m *CreateJobResponse) String() string { return proto.CompactTextString(m) }
-func (*CreateJobResponse) ProtoMessage()    {}
-func (*CreateJobResponse) Descriptor() ([]byte, []int) {
-	return fileDescriptor_hybrik_552458008a7cdb9c, []int{4}
+func (m *CreateJobOut) Reset()         { *m = CreateJobOut{} }
+func (m *CreateJobOut) String() string { return proto.CompactTextString(m) }
+func (*CreateJobOut) ProtoMessage()    {}
+func (*CreateJobOut) Descriptor() ([]byte, []int) {
+	return fileDescriptor_hybrik_8fee990f0d0ce65a, []int{4}
 }
-func (m *CreateJobResponse) XXX_Unmarshal(b []byte) error {
-	return xxx_messageInfo_CreateJobResponse.Unmarshal(m, b)
+func (m *CreateJobOut) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_CreateJobOut.Unmarshal(m, b)
 }
-func (m *CreateJobResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	return xxx_messageInfo_CreateJobResponse.Marshal(b, m, deterministic)
+func (m *CreateJobOut) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_CreateJobOut.Marshal(b, m, deterministic)
 }
-func (dst *CreateJobResponse) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_CreateJobResponse.Merge(dst, src)
+func (dst *CreateJobOut) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_CreateJobOut.Merge(dst, src)
 }
-func (m *CreateJobResponse) XXX_Size() int {
-	return xxx_messageInfo_CreateJobResponse.Size(m)
+func (m *CreateJobOut) XXX_Size() int {
+	return xxx_messageInfo_CreateJobOut.Size(m)
 }
-func (m *CreateJobResponse) XXX_DiscardUnknown() {
-	xxx_messageInfo_CreateJobResponse.DiscardUnknown(m)
+func (m *CreateJobOut) XXX_DiscardUnknown() {
+	xxx_messageInfo_CreateJobOut.DiscardUnknown(m)
 }
 
-var xxx_messageInfo_CreateJobResponse proto.InternalMessageInfo
+var xxx_messageInfo_CreateJobOut proto.InternalMessageInfo
 
-func (m *CreateJobResponse) GetJobId() string {
+func (m *CreateJobOut) GetJobId() string {
 	if m != nil {
 		return m.JobId
 	}
@@ -262,11 +262,11 @@ func (m *CreateJobResponse) GetJobId() string {
 }
 
 func init() {
-	proto.RegisterType((*JobRequest)(nil), "hybrik.JobRequest")
-	proto.RegisterType((*JobResponse)(nil), "hybrik.JobResponse")
+	proto.RegisterType((*JobIn)(nil), "hybrik.JobIn")
+	proto.RegisterType((*JobOut)(nil), "hybrik.JobOut")
 	proto.RegisterType((*Job)(nil), "hybrik.Job")
-	proto.RegisterType((*CreateJobRequest)(nil), "hybrik.CreateJobRequest")
-	proto.RegisterType((*CreateJobResponse)(nil), "hybrik.CreateJobResponse")
+	proto.RegisterType((*CreateJobIn)(nil), "hybrik.CreateJobIn")
+	proto.RegisterType((*CreateJobOut)(nil), "hybrik.CreateJobOut")
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -281,8 +281,8 @@ const _ = grpc.SupportPackageIsVersion4
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type HybrikClient interface {
-	GetJob(ctx context.Context, in *JobRequest, opts ...grpc.CallOption) (*JobResponse, error)
-	CreateJob(ctx context.Context, in *CreateJobRequest, opts ...grpc.CallOption) (*CreateJobResponse, error)
+	GetJob(ctx context.Context, in *JobIn, opts ...grpc.CallOption) (*JobOut, error)
+	CreateJob(ctx context.Context, in *CreateJobIn, opts ...grpc.CallOption) (*CreateJobOut, error)
 }
 
 type hybrikClient struct {
@@ -293,8 +293,8 @@ func NewHybrikClient(cc *grpc.ClientConn) HybrikClient {
 	return &hybrikClient{cc}
 }
 
-func (c *hybrikClient) GetJob(ctx context.Context, in *JobRequest, opts ...grpc.CallOption) (*JobResponse, error) {
-	out := new(JobResponse)
+func (c *hybrikClient) GetJob(ctx context.Context, in *JobIn, opts ...grpc.CallOption) (*JobOut, error) {
+	out := new(JobOut)
 	err := c.cc.Invoke(ctx, "/hybrik.Hybrik/GetJob", in, out, opts...)
 	if err != nil {
 		return nil, err
@@ -302,8 +302,8 @@ func (c *hybrikClient) GetJob(ctx context.Context, in *JobRequest, opts ...grpc.
 	return out, nil
 }
 
-func (c *hybrikClient) CreateJob(ctx context.Context, in *CreateJobRequest, opts ...grpc.CallOption) (*CreateJobResponse, error) {
-	out := new(CreateJobResponse)
+func (c *hybrikClient) CreateJob(ctx context.Context, in *CreateJobIn, opts ...grpc.CallOption) (*CreateJobOut, error) {
+	out := new(CreateJobOut)
 	err := c.cc.Invoke(ctx, "/hybrik.Hybrik/CreateJob", in, out, opts...)
 	if err != nil {
 		return nil, err
@@ -313,8 +313,8 @@ func (c *hybrikClient) CreateJob(ctx context.Context, in *CreateJobRequest, opts
 
 // HybrikServer is the server API for Hybrik service.
 type HybrikServer interface {
-	GetJob(context.Context, *JobRequest) (*JobResponse, error)
-	CreateJob(context.Context, *CreateJobRequest) (*CreateJobResponse, error)
+	GetJob(context.Context, *JobIn) (*JobOut, error)
+	CreateJob(context.Context, *CreateJobIn) (*CreateJobOut, error)
 }
 
 func RegisterHybrikServer(s *grpc.Server, srv HybrikServer) {
@@ -322,7 +322,7 @@ func RegisterHybrikServer(s *grpc.Server, srv HybrikServer) {
 }
 
 func _Hybrik_GetJob_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(JobRequest)
+	in := new(JobIn)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
@@ -334,13 +334,13 @@ func _Hybrik_GetJob_Handler(srv interface{}, ctx context.Context, dec func(inter
 		FullMethod: "/hybrik.Hybrik/GetJob",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(HybrikServer).GetJob(ctx, req.(*JobRequest))
+		return srv.(HybrikServer).GetJob(ctx, req.(*JobIn))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
 func _Hybrik_CreateJob_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(CreateJobRequest)
+	in := new(CreateJobIn)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
@@ -352,7 +352,7 @@ func _Hybrik_CreateJob_Handler(srv interface{}, ctx context.Context, dec func(in
 		FullMethod: "/hybrik.Hybrik/CreateJob",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(HybrikServer).CreateJob(ctx, req.(*CreateJobRequest))
+		return srv.(HybrikServer).CreateJob(ctx, req.(*CreateJobIn))
 	}
 	return interceptor(ctx, in, info, handler)
 }
@@ -374,28 +374,28 @@ var _Hybrik_serviceDesc = grpc.ServiceDesc{
 	Metadata: "hybrik.proto",
 }
 
-func init() { proto.RegisterFile("hybrik.proto", fileDescriptor_hybrik_552458008a7cdb9c) }
+func init() { proto.RegisterFile("hybrik.proto", fileDescriptor_hybrik_8fee990f0d0ce65a) }
 
-var fileDescriptor_hybrik_552458008a7cdb9c = []byte{
-	// 320 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x6c, 0x51, 0xc1, 0x4e, 0xeb, 0x30,
-	0x10, 0x7c, 0x6e, 0x5a, 0x3f, 0xba, 0x41, 0xa8, 0x2c, 0x02, 0xb9, 0x95, 0x90, 0xaa, 0x70, 0xa9,
-	0x2a, 0x94, 0x43, 0x7b, 0xe1, 0x5c, 0x0e, 0x40, 0x4f, 0x55, 0x7e, 0x00, 0xc5, 0xd8, 0xa5, 0x2e,
-	0x24, 0x2e, 0xb6, 0x7b, 0xe0, 0xc8, 0x37, 0xf0, 0xc3, 0xc8, 0x4e, 0x1a, 0x05, 0xd4, 0xdb, 0xce,
-	0xec, 0x78, 0x76, 0x32, 0x81, 0xd3, 0xcd, 0x27, 0x37, 0xea, 0x2d, 0xdd, 0x19, 0xed, 0x34, 0xd2,
-	0x0a, 0x25, 0x37, 0x00, 0x4b, 0xcd, 0x33, 0xf9, 0xb1, 0x97, 0xd6, 0xe1, 0x25, 0xd0, 0xad, 0xe6,
-	0xcf, 0x4a, 0x30, 0x32, 0x26, 0x93, 0x7e, 0xd6, 0xdb, 0x6a, 0xfe, 0x24, 0x92, 0x5b, 0x88, 0x83,
-	0xc8, 0xee, 0x74, 0x69, 0x25, 0x5e, 0x43, 0xb4, 0xd5, 0x3c, 0x48, 0xe2, 0x59, 0x9c, 0xd6, 0xbe,
-	0x5e, 0xe1, 0xf9, 0xe4, 0x9b, 0x40, 0xb4, 0xd4, 0x1c, 0xcf, 0xa0, 0xd3, 0x18, 0x75, 0x94, 0x40,
-	0x84, 0x6e, 0x99, 0x17, 0x92, 0x75, 0x02, 0x13, 0x66, 0xbc, 0x02, 0x6a, 0x5d, 0xee, 0xf6, 0x96,
-	0x45, 0x81, 0xad, 0x11, 0x32, 0xf8, 0x6f, 0x5d, 0x6e, 0x9c, 0x14, 0xac, 0x3b, 0x26, 0x93, 0x28,
-	0x3b, 0x40, 0x1c, 0xc1, 0xc9, 0x5a, 0x95, 0xca, 0x6e, 0xa4, 0x60, 0xbd, 0xb0, 0x6a, 0xb0, 0xdf,
-	0xed, 0x8c, 0x7e, 0x35, 0xd2, 0x5a, 0x46, 0xc7, 0x64, 0xd2, 0xcb, 0x1a, 0x9c, 0xdc, 0xc1, 0xe0,
-	0xde, 0xc8, 0xdc, 0xc9, 0xd6, 0xe7, 0x0e, 0x20, 0x12, 0xca, 0xd4, 0x11, 0xfd, 0xe8, 0x33, 0xae,
-	0xd5, 0x7b, 0x93, 0xd1, 0xcf, 0xc9, 0x14, 0xce, 0x5b, 0x2f, 0xeb, 0x0e, 0x8e, 0x37, 0x35, 0xfb,
-	0x22, 0x40, 0x1f, 0x43, 0x1f, 0x38, 0x07, 0xfa, 0x20, 0x9d, 0x2f, 0x02, 0xdb, 0x15, 0x55, 0xa7,
-	0x47, 0x17, 0xbf, 0xb8, 0xca, 0x34, 0xf9, 0x87, 0x0b, 0xe8, 0x37, 0xb7, 0x90, 0x1d, 0x34, 0x7f,
-	0x83, 0x8f, 0x86, 0x47, 0x36, 0x07, 0x8f, 0xc5, 0x14, 0x86, 0x2f, 0xba, 0x48, 0x4b, 0x59, 0xba,
-	0xb4, 0xc8, 0x8b, 0xea, 0x87, 0xd7, 0x0f, 0x16, 0x71, 0x95, 0x6e, 0xe5, 0xb9, 0x15, 0xe1, 0x34,
-	0x2c, 0xe7, 0x3f, 0x01, 0x00, 0x00, 0xff, 0xff, 0xaa, 0xcd, 0x00, 0x57, 0x1d, 0x02, 0x00, 0x00,
+var fileDescriptor_hybrik_8fee990f0d0ce65a = []byte{
+	// 314 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x6c, 0x91, 0xbf, 0x4e, 0xc3, 0x30,
+	0x10, 0xc6, 0x49, 0xd3, 0x18, 0x7a, 0x29, 0x15, 0x3a, 0xfe, 0xc8, 0x54, 0x02, 0x55, 0x91, 0x10,
+	0x85, 0x21, 0x43, 0xbb, 0x30, 0x97, 0x01, 0x9a, 0xa5, 0x55, 0x5e, 0x00, 0xc5, 0xc4, 0xa5, 0x2e,
+	0x24, 0xae, 0x6c, 0x77, 0xe0, 0x39, 0x78, 0x61, 0x64, 0x27, 0xb5, 0x2a, 0xc1, 0x76, 0xbf, 0xef,
+	0xf3, 0x9d, 0xbe, 0x3b, 0x43, 0x7f, 0xfd, 0xcd, 0x94, 0xf8, 0x4c, 0xb7, 0x4a, 0x1a, 0x89, 0xa4,
+	0xa1, 0xe4, 0x16, 0xa2, 0x4c, 0xb2, 0x79, 0x8d, 0x97, 0x40, 0x36, 0x92, 0xbd, 0x89, 0x92, 0x06,
+	0xa3, 0x60, 0xdc, 0xcb, 0xa3, 0x8d, 0x64, 0xf3, 0x32, 0xb9, 0x07, 0x92, 0x49, 0xb6, 0xd8, 0x19,
+	0xbc, 0x81, 0x70, 0x23, 0x99, 0x73, 0xe3, 0x49, 0x9c, 0xb6, 0xd3, 0x32, 0xc9, 0x72, 0xab, 0x27,
+	0x3f, 0x01, 0x84, 0x99, 0x64, 0x38, 0x80, 0x8e, 0x9f, 0xd1, 0x11, 0x25, 0x22, 0x74, 0xeb, 0xa2,
+	0xe2, 0xb4, 0xe3, 0x14, 0x57, 0xe3, 0x15, 0x10, 0x6d, 0x0a, 0xb3, 0xd3, 0x34, 0x74, 0x6a, 0x4b,
+	0x48, 0xe1, 0x58, 0x9b, 0x42, 0x19, 0x5e, 0xd2, 0xee, 0x28, 0x18, 0x87, 0xf9, 0x1e, 0x71, 0x08,
+	0x27, 0x2b, 0x51, 0x0b, 0xbd, 0xe6, 0x25, 0x8d, 0x9c, 0xe5, 0xd9, 0x7a, 0x5b, 0x25, 0x3f, 0x14,
+	0xd7, 0x9a, 0x92, 0x51, 0x30, 0x8e, 0x72, 0xcf, 0xc9, 0x14, 0xe2, 0x67, 0xc5, 0x0b, 0xc3, 0x9b,
+	0x25, 0xcf, 0x20, 0x2c, 0x85, 0x6a, 0xd3, 0xd9, 0xd2, 0xc6, 0x5b, 0x89, 0x2f, 0x1f, 0xcf, 0xd6,
+	0xc9, 0x1d, 0xf4, 0x7d, 0x93, 0xdd, 0xfc, 0xff, 0xd3, 0x4c, 0x2a, 0x20, 0xaf, 0xee, 0x08, 0xf8,
+	0x00, 0xe4, 0x85, 0x1b, 0xbb, 0xfd, 0xe9, 0xc1, 0x5d, 0xe6, 0xf5, 0x70, 0x70, 0x80, 0x8b, 0x9d,
+	0x49, 0x8e, 0xf0, 0x09, 0x7a, 0x7e, 0x36, 0x9e, 0xef, 0xed, 0x83, 0x8c, 0xc3, 0x8b, 0x3f, 0xa2,
+	0xeb, 0x9c, 0x3d, 0xc2, 0xf5, 0xbb, 0xac, 0xd2, 0x9a, 0xd7, 0x26, 0xad, 0x8a, 0xaa, 0xf9, 0xc7,
+	0xf6, 0xed, 0x2c, 0x6e, 0x92, 0x2c, 0xad, 0xb6, 0x0c, 0x18, 0x71, 0xe6, 0xf4, 0x37, 0x00, 0x00,
+	0xff, 0xff, 0x03, 0x31, 0x75, 0x29, 0xf4, 0x01, 0x00, 0x00,
 }
